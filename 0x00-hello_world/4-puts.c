be0760a7358ea1c250b36075f56cfd945916485e
OforiAlexander <alexanderofori07@gmail.com>
2023-06-15 15:25:49 -0700

    first output using C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - My first program that outputs a line of text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle/n");
+    return (0);
+}
