540097e2fd4f1d7250f019555358444cf23b3376
OforiAlexander <alexanderofori07@gmail.com>
2023-06-16 15:53:01 -0700

    commited hexa

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 /**
- * main - printing Hexadecimal charaxters
+ * main - hexadecimal looks scary so we are outputiing it by loops
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	int a;
-
-	char b;
-
-	a = 0;
-	b = 'a';
-	while
-		(a < 10)
-		{
-			putchar(a + '0');
-			a++;
-		}
-	while
-		(b <= 16)
-		{
-			putchar(b);
-			b++;
-		}
-	putchar('\n');
-	return (0);
+int a;
+char b;
+a = 0;
+b = 'a';
+while (a < 10)
+{
+putchar('0' + a);
+a++;
 }
+while (b <= 'f')
+{
+putchar(b);
+b++;
+}
+return (0);
+}
+
