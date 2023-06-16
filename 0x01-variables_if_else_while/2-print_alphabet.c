675bcdfc5003f7382ce3df8cd365090073d4c549
OforiAlexander <alexanderofori07@gmail.com>
2023-06-16 10:10:49 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - This task requires that we loop over alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) {
+    for (char a = 'a'; a <= 'z'; a++) {
+        putchar(a);
+    }
+    putchar('\n');
+    return 0;
+}
