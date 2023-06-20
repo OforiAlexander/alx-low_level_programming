65503c862ed2571e2dd5989c2f95317902624b1d
OforiAlexander <alexanderofori07@gmail.com>
2023-06-20 15:22:11 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - trying our hands on the c functions and nested loops
+ *
+ * Return: Always 0 (success)
+ *
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
