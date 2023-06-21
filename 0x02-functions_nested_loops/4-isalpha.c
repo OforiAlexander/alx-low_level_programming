fbdbd51c7b4c30067e51717664f7c74d43db2382
OforiAlexander <alexanderofori07@gmail.com>
2023-06-20 18:50:34 -0700

    commited

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * int _isalph - returns a response with char is lower or upper
+ * _isalpha - this checks for alphabetical characters
  * @c: the int to be changed
- * Return: 1 if c is a letter, 0 otherwise
+ * Return: 1 if int is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c >= 'z') || (c >= 'A' && c <= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+
 }
