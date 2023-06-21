480d02386d051a234ac1cfb93e154c12e3c59d66
OforiAlexander <alexanderofori07@gmail.com>
2023-06-20 20:23:21 -0700

    some diffualtm codes do not work

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - add two numbers together using a function in c
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: Sum of a and b
+ */
+int add(int a, int b)
+{
+	int d;
+
+	d = a * b;
+
+	return (d);
+}
