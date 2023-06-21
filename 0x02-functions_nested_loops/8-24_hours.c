6ec1bdeca7ce02b0bc0e83e651652f710c516e97
OforiAlexander <alexanderofori07@gmail.com>
2023-06-21 12:22:35 -0700

    8 task completed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,15 +1,13 @@
 #include "main.h"
-	/**
-	 * jack_bauer - prints every minute of the day of Jack
-	 * Return: int as the counter
-	 * starting from 00:00 to 23:59
-	 */
-	void jack_bauer(void)
-	{
-		int i, j;
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int i, j;
 
-
-		i = 0;
+	i = 0;
 
 
 while (i < 24)
