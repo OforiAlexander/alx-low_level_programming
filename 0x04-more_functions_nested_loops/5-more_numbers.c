003cfea4f95c03509deab310e226d2ada07c3e13
OforiAlexander <alexanderofori07@gmail.com>
2023-06-22 14:47:40 -0700

    updates made

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,16 @@
  */
 void more_numbers(void)
 {
-	int c, j;
+int c, j;
 
-	for (c = 0; c < 10; c++)
+for (c = 0; c < 10; c++)
 	{
-		for (j = 0; j < 15; j++)
+for (j = 0; j < 15; j++)
 		{
-			if (j >= 10)
- _putchar(j / 10 + '0');
- _putchar(j % 10 + '0');
+if (j >= 10)
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
 		}
-		_putchar('\n');
+_putchar('\n');
 	}
 }
